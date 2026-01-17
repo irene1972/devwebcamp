19f3f611fb29a6ec0b902321f8eded24beadadd7
Irene Olmos Gomez <ireneog_72@hotmail.es>
2026-01-17 21:37:40 +0100

    add estilos y html al login

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -2,10 +2,14 @@ import { Routes } from '@angular/router';
 import { Login } from './components/login/login';
 import { Main } from './components/main/main';
 import { C404 } from './components/c404/c404';
+import { Registro } from './components/registro/registro';
+import { Olvide } from './components/olvide/olvide';
 
 export const routes: Routes = [
     {path:'',pathMatch:'full',redirectTo:'home'},
     {path:'home',component:Main},
     {path:'login',component:Login},
+    {path:'registro',component:Registro},
+    {path:'olvide',component:Olvide},
     {path:'**',component:C404}
 ];
