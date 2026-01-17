fc371be6a3704c3ffd51990f433dd93d0fc7466d
Irene Olmos Gomez <ireneog_72@hotmail.es>
2026-01-17 13:48:40 +0100

    add components header,footer,main y login

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { Login } from './components/login/login';
+import { Main } from './components/main/main';
+import { C404 } from './components/c404/c404';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:'',pathMatch:'full',redirectTo:'home'},
+    {path:'home',component:Main},
+    {path:'login',component:Login},
+    {path:'**',component:C404}
+];
