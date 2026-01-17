113de91967304fcf76964da2d07e9d375f228d7d
Irene Olmos Gomez <ireneog_72@hotmail.es>
2026-01-17 21:36:37 +0100

    add nuevos componentes olvide y registro

---
+++ frontend/src/app/components/olvide/olvide.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-olvide',
+  imports: [],
+  templateUrl: './olvide.html',
+  styleUrl: './olvide.css',
+})
+export class Olvide {
+
+}
