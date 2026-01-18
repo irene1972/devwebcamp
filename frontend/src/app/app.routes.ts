42aee0e3c4be96acbced61f7ae66718cd4f96ed1
Irene Olmos Gomez <ireneog_72@hotmail.es>
2026-01-18 10:46:29 +0100

    crear componente cuenta-creada

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -4,6 +4,7 @@ import { Main } from './components/main/main';
 import { C404 } from './components/c404/c404';
 import { Registro } from './components/registro/registro';
 import { Olvide } from './components/olvide/olvide';
+import { CuentaCreada } from './components/cuenta-creada/cuenta-creada';
 
 export const routes: Routes = [
     {path:'',pathMatch:'full',redirectTo:'home'},
@@ -11,5 +12,7 @@ export const routes: Routes = [
     {path:'login',component:Login},
     {path:'registro',component:Registro},
     {path:'olvide',component:Olvide},
+    {path:'cuenta-creada',component:CuentaCreada},
+
     {path:'**',component:C404}
 ];
---
+++ frontend/src/app/components/cuenta-creada/cuenta-creada.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cuenta-creada',
+  imports: [],
+  templateUrl: './cuenta-creada.html',
+  styleUrl: './cuenta-creada.css',
+})
+export class CuentaCreada {
+
+}
