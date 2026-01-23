dca7e6fdf34e103776dd6a973c8051d549c1b70f
Irene Olmos Gomez <ireneog_72@hotmail.es>
2026-01-23 08:52:17 +0100

    add paginación

--- frontend/src/app/components/ponentes/ponentes.ts
+++ frontend/src/app/components/ponentes/ponentes.ts
@@ -5,62 +5,93 @@ import { environment } from '../../../environments/environment';
 
 @Component({
   selector: 'app-ponentes',
-  imports: [ReactiveFormsModule,RouterLink,RouterLinkActive],
+  imports: [ReactiveFormsModule, RouterLink, RouterLinkActive],
   templateUrl: './ponentes.html',
   styleUrl: './ponentes.css',
 })
 export class Ponentes {
-  miForm:FormGroup;
-  titulo:string='Ponentes / Conferencistas';
-  mensaje:string='';
-  tipo:boolean=false;
-  arrayPonentes:any=[];
+  miForm: FormGroup;
+  titulo: string = 'Ponentes / Conferencistas';
+  mensaje: string = '';
+  tipo: boolean = false;
+  arrayPonentes: any = [];
 
-  constructor(private cd: ChangeDetectorRef,private router:Router){
-    this.miForm=new FormGroup({},[]);
+  //añadir paginación
+  ponentesPaginados: any[] = [];
+
+  paginaActual: number = 1;
+  itemsPorPagina: number = 5;
+  totalPaginas: number = 0;
+
+  constructor(private cd: ChangeDetectorRef, private router: Router) {
+    this.miForm = new FormGroup({}, []);
   }
 
   ngOnInit(): void {
-    fetch(`${environment.apiUrl}api/ponente/listar`,{
-      method:'GET'
+    fetch(`${environment.apiUrl}api/ponente/listar`, {
+      method: 'GET'
     })
-      .then(response=>response.json())
-      .then(data=>{
+      .then(response => response.json())
+      .then(data => {
         //console.log(data);
-        if(data.error){
-          this.mensaje=data.error;
+        if (data.error) {
+          this.mensaje = data.error;
           return;
         }
-        if(data.length===0){
-          this.mensaje='No hay datos de ponentes';
-          this.tipo=true;
-        }else{
-          this.arrayPonentes=data;
+        if (data.length === 0) {
+          this.mensaje = 'No hay datos de ponentes';
+          this.tipo = true;
+        } else {
+          this.arrayPonentes = data;
+
+          //añadir paginación
+          this.totalPaginas = Math.ceil(this.arrayPonentes.length / this.itemsPorPagina);
+          this.actualizarPaginacion();
         }
       })
-      .catch(error=>console.log(error))
+      .catch(error => console.log(error))
       .finally(() => {
         this.cd.detectChanges();
       });
   }
 
-  cerrarSesion(){
+  cerrarSesion() {
     console.log('cerrar sesion');
   }
-  
-  eliminar(event:Event,id:number){
+
+  eliminar(event: Event, id: number) {
     event.preventDefault();
-    const boton=event.target as HTMLInputElement;
-    
-    fetch(`${environment.apiUrl}api/ponente/eliminar/${id}`,{
-      method:'DELETE'
+    const boton = event.target as HTMLInputElement;
+
+    fetch(`${environment.apiUrl}api/ponente/eliminar/${id}`, {
+      method: 'DELETE'
     })
-      .then(response=>response.json())
-      .then(data=>{
+      .then(response => response.json())
+      .then(data => {
         console.log(data);
         //reload
         window.location.reload();
       })
-      .catch(error=>console.log(error));
+      .catch(error => console.log(error));
+  }
+
+  actualizarPaginacion() {
+    const inicio = (this.paginaActual - 1) * this.itemsPorPagina;
+    const fin = inicio + this.itemsPorPagina;
+    this.ponentesPaginados = this.arrayPonentes.slice(inicio, fin);
+  }
+
+  paginaAnterior() {
+    if (this.paginaActual > 1) {
+      this.paginaActual--;
+      this.actualizarPaginacion();
+    }
+  }
+
+  paginaSiguiente() {
+    if (this.paginaActual < this.totalPaginas) {
+      this.paginaActual++;
+      this.actualizarPaginacion();
+    }
   }
 }
