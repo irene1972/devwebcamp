3140525828d7ecbe1ad7f6b89b3470ec9e88c6f3
Irene Olmos Gomez <ireneog_72@hotmail.es>
2026-01-27 21:53:18 +0100

    add funcionalidad speakers para main

--- frontend/src/app/components/speakers/speakers.ts
+++ frontend/src/app/components/speakers/speakers.ts
@@ -1,31 +1,46 @@
 import { ChangeDetectorRef, Component } from '@angular/core';
 import { environment } from '../../../environments/environment';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-speakers',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './speakers.html',
   styleUrl: './speakers.css',
 })
 export class Speakers {
-  imagesUrl:string=environment.imagesUrl;
-  ponentes:any[]=[];
-  constructor(private cd: ChangeDetectorRef){}
+  imagesUrl: string = environment.imagesUrl;
+  ponentes: any[] = [];
+  arrayRedes:any = [];
 
-  ngOnInit(){
+  constructor(private cd: ChangeDetectorRef) { }
+
+  ngOnInit() {
     this.obtenerPonentes();
+
+
   }
 
-  obtenerPonentes(){
-    fetch(`${environment.apiUrl}api/ponente/listar`,{
-      method:'GET'
+  obtenerPonentes() {
+    fetch(`${environment.apiUrl}api/ponente/listar`, {
+      method: 'GET'
     })
-      .then(response=>response.json())
-      .then(data=>{
+      .then(response => response.json())
+      .then(data => {
         console.log(data);
-        this.ponentes=data;
+        this.ponentes = data;
+
+        for (let ponente of this.ponentes) {
+          const infoRedes:any={};
+          infoRedes.id=ponente.id;
+          const redesObj=JSON.parse(ponente.redes);
+          infoRedes.redes=Object.entries(redesObj);
+          this.arrayRedes.push(infoRedes);
+        }
+        console.log('ireneeee');
+        console.log(this.arrayRedes);
       })
-      .catch(error=>console.log(error))
+      .catch(error => console.log(error))
       .finally(() => {
         this.cd.detectChanges();
       });
