7bd5714934eef4949ca5483dde13e2a4b38e7e24
Irene Olmos Gomez <ireneog_72@hotmail.es>
2026-01-27 22:41:07 +0100

    add mapa al main

---
+++ frontend/src/app/components/mapa/mapa.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import * as L from 'leaflet';
+
+@Component({
+  selector: 'app-mapa',
+  imports: [],
+  templateUrl: './mapa.html',
+  styleUrl: './mapa.css',
+})
+export class Mapa {
+  ngAfterViewInit() {
+    const map = L.map('map').setView([40.4168, -3.7038], 13);
+
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution: '© OpenStreetMap'
+    }).addTo(map);
+  }
+}
