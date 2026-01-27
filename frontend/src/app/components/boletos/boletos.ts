4eb390c22451d85605a4245e2bc1013210990064
Irene Olmos Gomez <ireneog_72@hotmail.es>
2026-01-27 22:46:40 +0100

    crear el componente boletos

---
+++ frontend/src/app/components/boletos/boletos.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-boletos',
+  imports: [],
+  templateUrl: './boletos.html',
+  styleUrl: './boletos.css',
+})
+export class Boletos {
+
+}
--- frontend/src/app/components/main/main.ts
+++ frontend/src/app/components/main/main.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { Conferencias } from '../conferencias/conferencias';
 import { Resumen } from "../resumen/resumen";
 import { Speakers } from '../speakers/speakers';
+import { Boletos } from '../boletos/boletos';
 
 @Component({
   selector: 'app-main',
-  imports: [Conferencias, Resumen,Speakers],
+  imports: [Conferencias, Resumen, Speakers, Boletos],
   templateUrl: './main.html',
   styleUrl: './main.css',
 })
