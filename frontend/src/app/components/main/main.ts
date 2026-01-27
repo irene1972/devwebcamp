768fa5efed507070dd77a7a29af383acbeac25b8
Irene Olmos Gomez <ireneog_72@hotmail.es>
2026-01-27 12:45:13 +0100

    add componente speaker

--- frontend/src/app/components/main/main.ts
+++ frontend/src/app/components/main/main.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { Conferencias } from '../conferencias/conferencias';
 import { Resumen } from "../resumen/resumen";
+import { Speakers } from '../speakers/speakers';
 
 @Component({
   selector: 'app-main',
-  imports: [Conferencias, Resumen],
+  imports: [Conferencias, Resumen,Speakers],
   templateUrl: './main.html',
   styleUrl: './main.css',
 })
